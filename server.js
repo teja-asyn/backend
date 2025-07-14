e9d23385c5cbc453af415f4464d8d27cecec3c66
teja-asyn <saitejagummala1512@gmail.com>
2025-07-14 15:11:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 const express = require('express');
 require('dotenv').config();
+const cors = require('cors');
 const connectToDB = require('./src/db/db');
 const userModel = require('./src/models/user.model')
 
 const app = express();
 app.use(express.json())
+
+// app.use(cors());
+
+// app.use(cors({
+//   origin: 'https://nalaland.com/',
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+//   credentials: true
+// }));
+
+const allowedOrigins = ['https://nalaland.com/', 'http://localhost:8080'];
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
 
 
 app.post('/users',async (req,res)=>{
